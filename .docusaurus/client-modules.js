f0a83feb42aa747a3fbc10578e385601669a4cce
karthickp432001 <154499722+karthickp432001@users.noreply.github.com>
2024-04-03 11:53:39 +0530

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/karthick/Documents/iudx-developer-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/karthick/Documents/iudx-developer-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/karthick/Documents/iudx-developer-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/karthick/Documents/iudx-developer-docs/src/css/custom.css"),
-];
